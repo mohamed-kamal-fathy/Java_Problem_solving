47b44ceeaf657114aa58a2099353217ccead4e38
Mohamed Kamal <93957451+mohamed-kamal-fathy@users.noreply.github.com>
2025-06-03 06:12:48 +0300

    Add files via upload

---
+++ D1_lab1.java
@@ -0,0 +1,10 @@
+package src.Main;
+ class D1_lab1 {
+     public static void main(String[] args) {
+        if (args.length > 0) {
+            System.out.println(args[0]);
+        } else {
+            System.out.println("Hello Java");
+        }
+    }
+}
